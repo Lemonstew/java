d5bf39cf3f938a2307d9412dbb8135ec46b75ea4
Lemonstew <colt4949@naver.com>
2024-08-14 17:55:42 +0900

    숫자 작성

---
+++ src/ch02/lecture/p02type/C05Character.java
@@ -0,0 +1,15 @@
+package ch02.lecture.p02type;
+
+public class C05Character {
+    public static void main(String[] args) {
+        // char (2byte)
+        // 문자 하나
+
+        //unicode
+        char a = 'a'; //
+        char b = '가'; //
+        char c = '한';
+        char d = '8';
+        // char e = 'gk';
+    }
+}
